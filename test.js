3ef656f82115dc97814fa7a8ef37241050eae493
Martin <martin.capovcak@gmail.com>
2021-10-19 21:57:43 +0200

    Test version

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const alert = require('./index.js');
 
-alert({
-	type: `success`,
-	msg: `All done!`
-});
+alert();
+
+alert({type: `success`,msg: `All finished!`, name: `Wohoo`});
+
+alert({type: `warning`,msg: `You didn't to add something!`, name: `Watch out`});
+
+alert({type: `info`,msg: `What a cool day today!`, name: `Ehm`});
+
+alert({type: `error`,msg: `Ooops, something went wrong!`, name: `NOPE`});
 
 // ✔ SUCCESS  All done!
